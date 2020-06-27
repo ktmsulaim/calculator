b0c2b0d2a53ee838c0451f0433c02d9f6d4b639e
ktmsulaim <ktmsulaim@gmail.com>
2020-06-27 15:10:16 +0530

    Basic layout configured

---
+++ Base.py
@@ -0,0 +1,13 @@
+from tkinter import *
+# Instantiate the module
+root = Tk()
+# Basic layout configuration
+root.title("Calculator | Crossroads challenge")
+root.config(bg="#222")
+root.geometry("250x400+300+300")
+root.resizable(0,0)
+
+
+
+# Run application
+root.mainloop()
